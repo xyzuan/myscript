f3b93772a19795aaf62bbc3f4b29fe0f8533c4c3
diyanto <85061817+diyanto823@users.noreply.github.com>
2021-10-23 03:06:01 +0700

    Update compilekernel.sh

--- termux/compilekernel.sh
+++ termux/compilekernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export KBUILD_BUILD_USER=xyzuan
-export KBUILD_BUILD_HOST=xyzscape
+export KBUILD_BUILD_USER=ayy
+export KBUILD_BUILD_HOST=diyanto
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig
+    make -j$(nproc) O=out ARCH=arm64 ginkgo-perf_defconfig
     make -j$(nproc) ARCH=arm64 O=out \
                                CROSS_COMPILE=aarch64-linux-gnu- \
                                CROSS_COMPILE_ARM32=arm-linux-gnueabi-
